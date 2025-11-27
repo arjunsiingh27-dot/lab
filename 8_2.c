d90fa790dab8258dde35819629e2699b8a029c2f
arjunsiingh27-dot <arjunsiingh27@gmail.com>
2025-11-27 22:58:47 +0530

    Add files via upload

---
+++ 8_2.c
@@ -0,0 +1,24 @@
+// Perform pointer arithmetic (increment and decrement) on pointers of different
+// data types. Observe how the memory addresses change and the effects on
+// data access.
+#include<stdio.h>
+int main(){
+    int a = 10, *p;
+    float b = 4.5, *x;
+    char c = 'a', *y;
+    p = &a;
+    x = &b;
+    y = &c;
+    printf("value of p is :%d\n",*p);
+    printf("value of p is :%d\n",p);
+    printf("the address of p is %x\n ",&*p);
+    p++;
+    printf("value of p is :%d\n",*p);
+    printf("value of p is :%d\n",p); 
+    printf("the address of p is %x\n ",&*p);
+    p--;
+    printf("value of p is :%d\n",*p);
+    printf("value of p is :%d\n",p); 
+    printf("the address of p is %x\n ",&*p);
+    return 0;
+}
